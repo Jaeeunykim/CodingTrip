b4b77946e890aff3bb6e5be265a498ca7ac9ed2c
Jaeeunykim <jaeeun.kje@gmail.com>
2017-07-10 08:19:22 +0900

    add whitebookd chapters

--- Whitebook/Ch1/1_Getting_Started.c
+++ Whitebook/Ch1/1_Getting_Started.c
@@ -9,6 +9,13 @@ Hello, world 출력해보기
 int main() { } 
 코드의 시작점을 알려주기위한 Entry point인 int main() 을 입력한다
 
+c파일로 저장해주어야 한다  .c
+
+실행파일 생성 a.out
+
 printf("hello, world\n")
 'hello, world'를 출력하기위해 printf 함수를 호출한다
 \n는 newline의 약어이다
+
+예제1. 위에 만들었던 hello world를 부분 삭제 하고 실행 해보고 에러메시지 확인해봐라
+예제2. /를 포함해서 입출력 해봐라( \b, \c, \t)
